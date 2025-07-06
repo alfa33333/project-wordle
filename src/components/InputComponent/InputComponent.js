8141cd99d86595369bbd43a4dbbdbda319f5738e
Antonio Herrera Martin <44150484+alfa33333@users.noreply.github.com>
2025-07-06 12:56:35 -0600

    Implement InputComponent with controlled input and form submission

--- src/components/InputComponent/InputComponent.js
+++ src/components/InputComponent/InputComponent.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 function InputComponent() {
+  const [ word, setWord ] = React.useState("");
+
   return (
-    <form class="guess-input-wrapper">
+    <form class="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log({ word });
+        setWord("");
+      }}>
       <label for="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" />
+      <input 
+      id="guess-input" 
+      type="text"
+      required={true}
+      minLength={5}
+      value={word}
+      onChange={(event) => {
+        setWord(event.target.value.toUpperCase());}
+      } 
+      />
+      <button>Submit</button>
     </form>
   );
 }
